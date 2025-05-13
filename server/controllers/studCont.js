2ddaf90d2c5a4c2e93eb6f147eff622f04b68b52
Yaarabi <yaarabi318@gmail.com>
2025-05-13 17:39:29 +0100

    feat- create the controllers of users

---
+++ server/controllers/studCont.js
@@ -0,0 +1,54 @@
+
+import Users from "../models/student"
+
+
+const getDetailUser = async (req, res) => {
+    try {
+        const result =  await Users.find()
+        res.status(200).json(studs)
+    } catch (error) {
+        res.status(500).json({ message : "Error in fetching" })
+    }
+}
+
+const postUsers = async (req, res) => {
+    try {
+        const newUser = new Users(req.body);
+        await newUser.save()
+        res.status(200).json(studs)
+    } catch (error) {
+        res.status(500).json({ message : "Error in post" })
+    }
+}
+
+const putUser = async (req, res) => {
+    const {id} = req.parms
+    const detail = req.body
+    try {
+        const result = await Users.findByIdAndUpdate(id, detail, { new: true })
+        if(!result){
+            return  res.status(404).json({ message : "Not found" })
+        }
+        
+        res.status(200).json(result)
+    } catch (error) {
+        res.status(500).json({ message : "Error in put" })
+    }
+}
+
+const deletUser = async (req, res) => {
+    const {id} = req.parms
+    const detail = req.body
+    try {
+        const result = await Users.findByIdAndDelete(id, detail, { new: true })
+        if(!result){
+            return  res.status(404).json({ message : "Not found" })
+        }
+        
+        res.status(200).json(result)
+    } catch (error) {
+        res.status(500).json({ message : "Error in delete" })
+    }
+}
+
+export { getDetailUser, postUsers, putUser, deletUser }
