5eaee345c9fa3b709308caf239de5681a7ce2f2e
Yaarabi <yaarabi318@gmail.com>
2025-05-20 12:21:33 +0100

    test

---
+++ server/router/coursRouter.js
@@ -0,0 +1,23 @@
+
+import express from "express";
+import {
+  createCourse,
+  getAllCourses,
+  getCourseById,
+  updateCourse,
+  deleteCourse,
+} from "../controllers/courseController.js";
+
+const router = express.Router();
+
+router.post("/courses", createCourse);
+
+router.get("/courses", getAllCourses);
+
+router.get("/courses/:id", getCourseById);
+
+router.put("/courses/:id", updateCourse);
+
+router.delete("/courses/:id", deleteCourse);
+
+export default router;
