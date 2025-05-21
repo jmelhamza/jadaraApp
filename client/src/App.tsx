99a7d373068902153c03b476ced217ce7f5811a3
lahmaim <anas.lahmaim55@gmail.com>
2025-05-21 12:48:58 +0100

    creat nav bar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,16 @@
 
 // import MyForm from "./components/forms/registerForm"
 
-import MyForm from "./components/forms/registerForm"
+import { Navbar1 } from "./components/navbar1"
+
+// import MyForm from "./components/forms/registerForm"
 
 
 function App() {
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
-      <MyForm/>
+      {/* <MyForm/> */}
+     <Navbar1/>
     </div>
   )
 }
