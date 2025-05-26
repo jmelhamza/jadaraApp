19b024a6a2fb39b45d93e9465f4be0127ab4414f
jmelhamza <jmelhamza623@gmail.com>
2025-05-26 14:51:49 +0100

    setup shadcn

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,14 @@
 // import MyForm from "./components/forms/registerForm"
 
 import MyForm from "./components/forms/registerForm"
+import { Button } from "./components/ui/button"
 
 
 function App() {
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
       <MyForm/>
+      <Button/>
     </div>
   )
 }
