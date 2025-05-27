bf7cb7f8e89252ec4ba478b7dc72492408fca87f
essredouan <essoubairedouan@gmail.com>
2025-05-27 12:49:13 +0200

    add routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,22 @@
 
-import { Header } from "./components/header/header"
-import { Footer } from "./components/footer/footer"
-import { CarouselSection } from "./components/main/main"
+import { Header } from "./components/header/header";
+import { Footer } from "./components/footer/footer";
+import { CarouselSection } from "./components/main/main";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from "./components/HomePage";
+import Forms from "./components/Forms"; 
+
+// routes
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/Forms",
+    element: <Forms />, 
+  },
+]);
 
 function App() {
   return (
