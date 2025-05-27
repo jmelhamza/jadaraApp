0f501165a9bfe0a6c8e489fdc5025dd8a37a81a7
jmelhamza <jmelhamza623@gmail.com>
2025-05-27 21:24:35 +0100

    feching ebvents data events its empty

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,39 +1,13 @@
-import React from "react"
-import { Header } from "./components/header/header"
-import { Footer } from "./components/footer/footer"
-import { CarouselSection } from "./components/main/main"
-
-function App() {
-  return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <CarouselSection />
-      <Footer />
+import { CarouselSpacing } from "./components/events/event"
 
 
-// import MyForm from "./components/forms/registerForm"
 
-<<<<<<< HEAD
-import MyForm from "./components/forms/registerForm"
-import { Button } from "./components/ui/button"
-=======
-import Login from "./components/forms/loginForm"
-
-// import Layout from "./components/layout/Layout"
->>>>>>> dev
 
 
 function App() {
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
-<<<<<<< HEAD
-      <MyForm/>
-      <Button/>
-=======
-      {/* <MyForm/> */}
-      {/* <Layout/> */}
-      <Login/>
->>>>>>> dev
+      <CarouselSpacing/>
     </div>
   )
 }
