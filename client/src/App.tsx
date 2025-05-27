ba6264e8fe91b98bd2bd4bac25ee5bced3e3b557
essredouan <essoubairedouan@gmail.com>
2025-05-27 11:30:59 +0200

    r

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,3 @@
-import React from "react"
-import { Header } from "./components/header/header"
-import { Footer } from "./components/footer/footer"
-import { CarouselSection } from "./components/main/main"
-
-function App() {
-  return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <CarouselSection />
-      <Footer />
-
-
-// import MyForm from "./components/forms/registerForm"
-
-import Login from "./components/forms/loginForm"
 
 // import Layout from "./components/layout/Layout"
 
@@ -23,7 +7,7 @@ function App() {
     <div className="flex flex-col items-center justify-center min-h-svh">
       {/* <MyForm/> */}
       {/* <Layout/> */}
-      <Login/>
+      
     </div>
   )
 }
